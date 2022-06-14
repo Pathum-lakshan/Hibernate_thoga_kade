a8b3a4ea10299947a2617f2abecc3e04f58abf92
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-14 05:17:51 -0700

    complete Customer Crud

--- src/lk/ijse/hibernate/Main.java
+++ src/lk/ijse/hibernate/Main.java
@@ -1,5 +1,6 @@
 package lk.ijse.hibernate;
 
+import lk.ijse.hibernate.entity.Customer;
 import lk.ijse.hibernate.util.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -10,6 +11,38 @@ public class Main {
         Session session = FactoryConfiguration.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
+
+        //  Customer
+
+/** ===Create Customer====  */
+
+        Customer customer1 = new Customer("C001", "Pathum", "Panadura", "076 1236545");
+        Customer customer2 = new Customer("C002", "Lakshan", "colombo", "076 2546321");
+        Customer customer3 = new Customer("C003", "Herath", " kaluthara", "076 2587463");
+
+        session.save(customer1);
+        session.save(customer2);
+        session.save(customer3);
+
+/** ===Read Customer====  */
+
+        Customer c001 = session.get(Customer.class, "C001");
+        Customer c002 = session.get(Customer.class, "C002");
+
+        System.out.println(c001);
+        System.out.println(c002);
+
+/** ===Update Customer====  */
+
+        customer1.setCustomer_address("candy");
+        session.update(customer1);
+
+/** ===Delete Customer====  */
+
+        session.delete(session.get(Customer.class,"C003"));
+
+
+
 
 
 
