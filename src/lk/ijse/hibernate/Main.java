f8292d46ffab0b0973fe50361250a623c2af7d74
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-14 04:12:04 -0700

    create customer class

--- src/lk/ijse/hibernate/Main.java
+++ src/lk/ijse/hibernate/Main.java
@@ -1,8 +1,23 @@
 package lk.ijse.hibernate;
 
+import lk.ijse.hibernate.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Session session = FactoryConfiguration.getInstance().getSession();
+
+        Transaction transaction = session.beginTransaction();
+
+
+
+
+
+        transaction.commit();
+
+        session.close();
+
     }
 }
---
+++ src/lk/ijse/hibernate/entity/Customer.java
@@ -0,0 +1,11 @@
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hibernate_thoga_kade
+ * Date        : 6/14/2022
+ * Time        : 4:09 AM
+ */
+
+package lk.ijse.hibernate.entity;
+
+public class Customer {
+}
