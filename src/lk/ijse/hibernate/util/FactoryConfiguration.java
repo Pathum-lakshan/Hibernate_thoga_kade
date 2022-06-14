c77f515126909d38c7b1018ea32d688283d0c180
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-14 03:56:37 -0700

    create hibernate cfg xml file

---
+++ src/lk/ijse/hibernate/util/FactoryConfiguration.java
@@ -0,0 +1,12 @@
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hibernate_thoga_kade
+ * Date        : 6/14/2022
+ * Time        : 3:55 AM
+ * @Since : 0.1.0
+ */
+
+package lk.ijse.hibernate.util;
+
+public class FactoryConfiguration {
+}
